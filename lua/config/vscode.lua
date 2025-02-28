6afe5086595e415e6f8e5b9eda0bc34968b30971
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-02-28 10:50:03 +0000

    Add vscode extension config

---
+++ lua/config/vscode.lua
@@ -0,0 +1,5 @@
+if vim.g.vscode then
+    -- undo/REDO via vscode
+    vim.keymap.set("n","u","<Cmd>call VSCodeNotify('undo')<CR>")
+    vim.keymap.set("n","<C-r>","<Cmd>call VSCodeNotify('redo')<CR>")
+end
